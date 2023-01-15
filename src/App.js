4097da27b2fe9f92652b5545192b9fe48913541d
Shotiko11 <shotikokokilashvili9@gmail.com>
2023-01-15 16:33:24 +0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect} from 'react';
 
-function App() {
+
+const generateUsers = () => [
+  {id:1, name:"irakli"},
+  {id:2, name:"cotne"},
+  {id:3, name:"giorgi"},
+  {id:4, name:"erekle"},
+  {id:5, name:"demna"},
+  {id:6, name:"ana"},
+  {id:7, name:"salome"},
+  {id:8, name:"nini"},
+  {id:9, name:"maka"},
+  {id:10, name:"nato"},
+]
+
+const App = ()=> {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    setUsers(generateUsers())
+  }, [])
+
+  useEffect(() => {
+    document.title = `${users.length} users length`
+  }, [users])
+
+
+  const deleteButton = () => {
+    const randomIndex = Math.floor(Math.random() * users.length);
+  
+    setUsers ((prevUsers) => {
+      const newUseArray = prevUsers.filter(
+        (_, index) => index!==randomIndex
+      )
+      return newUseArray
+    })
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      {users.map ((user) => {
+        console.log(user.name);
+        return (
+          <React.Fragment key={user.id}>
+          <p>{user.name}</p>
+        </React.Fragment>
+        )
+        
+      })}
+      <button onClick={deleteButton}>Delete random</button>
+    </div>  
+    
   );
 }
+
+
+
 
 export default App;
